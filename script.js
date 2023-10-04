cd30bba8602299dbfd377223f1bd319b47e6dae9
Andreugc19 <agarcia20507@iesjoanramis.org>
2023-10-04 21:13:59 +0200

    Commit Metropolis Andreu Gacia

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// Exercici Calculadora de Notes
-// Declarar les variables
+// Exercici Metropolis
+const esCapital = true; // Si és una capital
+const nombreDeCiutadans = 200000; // El nombre de ciutadans de la ciutat
+const impostPerCiutada = 300.4; // L'impost mitjà mensual que paga un ciutadà
 
-let nomEstudiant = "Andreu";
-let notaExamen1 = 6.8;
-let notaExamen2 = 8.2;
-let notaProjecte = 7.4;
+const esMetropolis = (esCapital && nombreDeCiutadans > 100000) || (!esCapital && nombreDeCiutadans > 200000 && impostPerCiutada * 12 > 720000000);
 
-// Calcular la nota final
-const notaFinal = (notaExamen1 * 0.2) + (notaExamen2 * 0.2) + (notaProjecte * 0.6);
-
-// Mostrar les notes i la nota final per consola
-console.log("Estudiant: " + nomEstudiant);
-console.log("Nota de l'Examen 1: " + notaExamen1);
-console.log("Nota de l'Examen 2: " + notaExamen2);
-console.log("Nota del Projecte: " + notaProjecte);
-console.log("Nota Final: " + notaFinal);
-
-//  Determinar si l'estudiant ha aprovat
-if (notaFinal >= 7) {
-    console.log("Felicitats, " + nomEstudiant + "! Has aprovat el mòdul!");
-} else {
-    console.log("Ho sentim, " + nomEstudiant + ". Necessites millorar la teva nota per a aprovar el mòdul.");
-}
+console.log(esMetropolis); // Aquesta variable serà certa si la ciutat és una metròpolis segons les condicions donades
